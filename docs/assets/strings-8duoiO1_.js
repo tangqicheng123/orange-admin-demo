1924e0e055cdcb11ffd940ef5fde2508baf29ed6
tqc <1635409114@qq.com>
2026-08-09 04:13:29 +0800

    feat: Demo 多处体现接单交付（登录标签条+场景卡+定价ROI+侧边栏标语）

--- docs/assets/strings-8duoiO1_.js
+++
@@ -1,1 +0,0 @@
-import{bO as a}from"./index-CiJq6cTJ.js";const p=(e="")=>e.replace(/[|\\{}()[\]^$+*?.]/g,"\\$&").replace(/-/g,"\\x2d"),t=e=>a(e);export{t as c,p as e};
--- docs/assets/validator-DeadkQe3.js
+++
@@ -1,1 +0,0 @@
-import{bi as o}from"./index-CiJq6cTJ.js";const n=i=>["",...o].includes(i);export{n as i};
